810f6d0c4ba9a2aabc7a116b558d2531e7158b46
Mr.Wizard <github@gentlewizard.ca>
2023-12-01 08:17:47 -0800

    part 2 Complete

--- first challange/part_two.py
+++ first challange/part_two.py
@@ -1,63 +1,70 @@
 import re
 import time
 
-START = time.time()
+time_start = time.time()
+
+words_to_numbers = {
+	'oneight':   18,
+	'fiveight':  58,
+	'threeight': 38,
+	'nineight':  98,
+	'twone':     21,
+	'sevenine':  79,
+	'eightwo':   82,
+	'one':       1,
+	'two':       2,
+	'three':     3,
+	'four':      4,
+	'five':      5,
+	'six':       6,
+	'seven':     7,
+	'eight':     8,
+	'nine':      9,
+}
 
 with open("codes.txt", "r") as f:
 	codes = f.readlines()
 
 
-def search_for_digits(string, words):
-	string = string.lower()
-	words = [word.lower() for word in words]
-	pattern = '(' + '|'.join(words) + '|\\d+)'
-	result = re.findall(pattern, string)
-	valid_digits = {
-		'one':   1,
-		'two':   2,
-		'three': 3,
-		'four':  4,
-		'five':  5,
-		'six':   6,
-		'seven': 7,
-		'eight': 8,
-		'nine':  9,
-		'zero':  0
-	}
+def concat_value(value):
+	return int(str(value[0]) + str(value[-1]))
+
+
+def splice_large_number(number):
+	return [int(digit) for digit in str(number)]
+
+
+def search_for_digits(string):
+	pattern = '(' + '|'.join(words_to_numbers.keys()) + '|\\d+)'
+	result = re.findall(pattern, string.lower())
+
 	answer = []
-	for str in result:
-		if str.isdigit():
-			answer.append(int(str))
-		elif str in valid_digits:
-			answer.append(valid_digits[str])
-
+	for string in result:
+		if string.isdigit():
+			if len(string) > 1:
+				spliced_digits = splice_large_number(string)
+				for digit in spliced_digits:
+					answer.append(digit)
+			else:
+				answer.append(int(string))
+		elif string in words_to_numbers:
+			if len(str(words_to_numbers[string])) > 1:
+				spliced_digits = splice_large_number(words_to_numbers[string])
+				for digit in spliced_digits:
+					answer.append(digit)
+			else:
+				answer.append(words_to_numbers[string])
 	return answer
-
-
-def split_large_numbers(digits):
-	return [int(digit) for digit in str(digits)]
 
 
 calibration_values = []
 for code in codes:
-	digits = search_for_digits(code, ['one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine', 'zero'])
-	i = 0
-	while i < len(digits):
-		number = digits[i]
-		if number > 9:
-			split_digits = split_large_numbers(number)
-			digits = digits[:i] + split_digits + digits[i + 1:]
-			i += len(split_digits)
-		else:
-			i += 1
+	digits = search_for_digits(code)
 
 	if digits:
-		calibration_value = int(str(digits[0]) + str(digits[-1]))
+		calibration_value = concat_value(digits)
 		calibration_values.append(calibration_value)
-	print(f"code: {code}")
-	print(f"digits: {digits}")
-	print(f"calibration_value: {calibration_value}")
 
 total_sum = sum(calibration_values)
 print(f"\ntotal_sum: {total_sum}")
-print(f"Time: {time.time() - START}")
+print(f"took {time.time() - time_start} seconds")
